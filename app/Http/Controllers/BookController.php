cf8fa00b19163501662d2dad48d34ab1f33c4ce5
Mason Tan <mtan810@gmail.com>
2016-04-12 19:22:39 -0400

    Update for lecture 10

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class BookController extends Controller {
 
@@ -10,33 +11,36 @@ class BookController extends Controller {
     * Responds to requests to GET /books
     */
     public function getIndex() {
-        return 'Show a list of all books';
+        return view('books.index');
     }
 
     /**
      * Responds to requests to GET /books/show/{id}
      */
-    public function getShow($id) {
-       return 'Show an individual book: ' . $id;
+    public function getShow($title = null) {
+        return view('books.show',[
+            'title' => $title,
+        ]);
     }
 
     /**
      * Responds to requests to GET /books/create
      */
     public function getCreate() {
-        $view = '<form method="POST" action="/book/create">';
-        $view .= csrf_field();
-        $view .= 'Book title: <input type="text" name="title">';
-        $view .= '<input type="submit">';
-        $view .= '</form>';
-        
-        return $view;
+        return view('books.create');
     }
 
     /**
      * Responds to requests to POST /books/create
      */
-    public function postCreate() {
-        return 'Add the book: '.$_POST['title'];
+    public function postCreate(Request $request) {
+        
+        $this->validate($request,[
+            'title' => 'required|min:3',
+            'author' => 'required'
+        ]);
+        
+        return 'Add the book: '.$request->input('title');
+        #return redirect('/books');
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,23 +23,58 @@
 | kernel and includes session state, CSRF protection, and more.
 |
 */
-
-Route::group(['middleware' => ['web']], function () {
     
-    Route::get('/', function () {
-        return view('welcome');
-    });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-    Route::get('/books', 'BookController@getIndex');
-    Route::get('/book/create', 'BookController@getCreate');
-    Route::post('/book/create', 'BookController@postCreate');
-    Route::get('/book/{id}', 'BookController@getShow');
+Route::get('/books', 'BookController@getIndex');
+Route::get('/book/create', 'BookController@getCreate');
+Route::post('/book/create', 'BookController@postCreate');
+Route::get('/book/show/{title?}', 'BookController@getShow');
+
+Route::get('/practice', function() {
     
-    Route::get('/practice', function() {
-        
-        $random = new Random();
-        return $random->getRandomString(10);
-        
-    });
+    $random = new Random();
+    return $random->getRandomString(10);
+    
+});
+
+if (App::environment('local')) {
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');  
+}
+
+Route::get('/debug', function() {
+
+    echo '<pre>';
+
+    echo '<h1>Environment</h1>';
+    echo App::environment().'</h1>';
+
+    echo '<h1>Debugging?</h1>';
+    if(config('app.debug')) echo "Yes"; else echo "No";
+
+    echo '<h1>Database Config</h1>';
+    /*
+    The following line will output your MySQL credentials.
+    Uncomment it only if you're having a hard time connecting to the database and you
+    need to confirm your credentials.
+    When you're done debugging, comment it back out so you don't accidentally leave it
+    running on your live server, making your credentials public.
+    */
+    //print_r(config('database.connections.mysql'));
+
+    echo '<h1>Test Database Connection</h1>';
+    try {
+        $results = DB::select('SHOW DATABASES;');
+        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+        echo "<br><br>Your Databases:<br><br>";
+        print_r($results);
+    }
+    catch (Exception $e) {
+        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+    }
+
+    echo '</pre>';
     
 });
